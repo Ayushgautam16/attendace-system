756863b04803be1d1aae57797bc4bdbc1c1c5a14
AYUSH GAUTAM <142661926+Ayushgautam16@users.noreply.github.com>
2024-08-03 20:43:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,21 @@ import numpy as np
 import csv
 import os
 from datetime import datetime
+
+video_capture = cv2.VideoCapture(0)
+ 
+jobs_image = face_recognition.load_image_file("photos/jobs.jpg")
+jobs_encoding = face_recognition.face_encodings(jobs_image)[0]
+ 
+ratan_tata_image = face_recognition.load_image_file("photos/tata.jpg")
+ratan_tata_encoding = face_recognition.face_encodings(ratan_tata_image)[0]
+ 
+sadmona_image = face_recognition.load_image_file("photos/sadmona.jpg")
+sadmona_encoding = face_recognition.face_encodings(sadmona_image)[0]
+ 
+tesla_image = face_recognition.load_image_file("photos/tesla.jpg")
+tesla_encoding = face_recognition.face_encodings(tesla_image)[0]
+ 
  
 
 known_face_encoding = [
