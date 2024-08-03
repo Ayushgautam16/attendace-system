73405ba5b5dda6faba490d97731334cb0816f5b2
AYUSH GAUTAM <142661926+Ayushgautam16@users.noreply.github.com>
2024-08-03 20:42:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -81,6 +81,21 @@ while True:
                     fontColor,
                     thickness,
                     lineType)
+
+
+     if name in students:
+                    students.remove(name)
+                    print(students)
+                    current_time = now.strftime("%H-%M-%S")
+                    lnwriter.writerow([name,current_time])
+    cv2.imshow("attendence system",frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+ 
+video_capture.release()
+cv2.destroyAllWindows()
+f.close()
+
  
               
  
